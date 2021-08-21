0d9e424983fdca5e231df599812f33e46d290228
BoyeGuillaume <guillaume.boye@epfl.ch>
2021-08-21 14:21:14 +0200

    fix: make code compatible with posix system (add ## before __VA_ARGS__ when empty args)

--- molog/src/molog/assertion.hpp
+++ molog/src/molog/assertion.hpp
@@ -9,17 +9,17 @@
 
 
 #define __MO_EXPAND_STR(str)                                                                       + ((std::string) str)
-#define MO_THROW_EXCEPTION(msg, ...)                                                               throw Molog::Exception(msg MO_FOR_EACH_P0(__MO_EXPAND_STR, "", __VA_ARGS__))
-#define MO_FORCE_ASSERT(condition, ...)                                                            do { if(!(condition)){ MO_THROW_EXCEPTION("assertion failed " MO_STRINGIZE(condition) ": ", __VA_ARGS__); }} while(0)
-#define MO_FORCE_ASSERT_NON_NULL(ptr, ...)                                                         MO_FORCE_ASSERT(ptr != nullptr, "NullPointerException ", __VA_ARGS__)
-#define MO_FORCE_ASSERT_NULL(ptr, ...)                                                             MO_FORCE_ASSERT(ptr == nullptr, "PointerAlreadyUsedException", __VA_ARGS__)
-#define MO_FORCE_ASSERT_RETURN_SUCCESS(op, ...)                                                    MO_FORCE_ASSERT(op == MO_SUCCESS, "OperationFailedException", __VA_ARGS__)
+#define MO_THROW_EXCEPTION(msg, ...)                                                               throw Molog::Exception(msg MO_FOR_EACH_P0(__MO_EXPAND_STR, "", ##__VA_ARGS__))
+#define MO_FORCE_ASSERT(condition, ...)                                                            do { if(!(condition)){ MO_THROW_EXCEPTION("assertion failed " MO_STRINGIZE(condition) ": ", ##__VA_ARGS__); }} while(0)
+#define MO_FORCE_ASSERT_NON_NULL(ptr, ...)                                                         MO_FORCE_ASSERT(ptr != nullptr, "NullPointerException ", ##__VA_ARGS__)
+#define MO_FORCE_ASSERT_NULL(ptr, ...)                                                             MO_FORCE_ASSERT(ptr == nullptr, "PointerAlreadyUsedException", ##__VA_ARGS__)
+#define MO_FORCE_ASSERT_RETURN_SUCCESS(op, ...)                                                    MO_FORCE_ASSERT(op == MO_SUCCESS, "OperationFailedException", ##__VA_ARGS__)
 
 #if defined(MOLOG_VALIDATION_LAYER)
- #define MO_ASSERT(condition, ...)                                                                 MO_FORCE_ASSERT(condition, __VA_ARGS__)
- #define MO_ASSERT_NON_NULL(ptr, ...)                                                              MO_FORCE_ASSERT_NON_NULL(ptr, __VA_ARGS__)
- #define MO_ASSERT_NULL(ptr, ...)                                                                  MO_FORCE_ASSERT_NULL(ptr, __VA_ARGS__)
- #define MO_ASSERT_RETURN_SUCCESS(op, ...)                                                         MO_FORCE_ASSERT_RETURN_SUCCESS(op, __VA_ARGS__)
+ #define MO_ASSERT(condition, ...)                                                                 MO_FORCE_ASSERT(condition, ##__VA_ARGS__)
+ #define MO_ASSERT_NON_NULL(ptr, ...)                                                              MO_FORCE_ASSERT_NON_NULL(ptr, ##__VA_ARGS__)
+ #define MO_ASSERT_NULL(ptr, ...)                                                                  MO_FORCE_ASSERT_NULL(ptr, ##__VA_ARGS__)
+ #define MO_ASSERT_RETURN_SUCCESS(op, ...)                                                         MO_FORCE_ASSERT_RETURN_SUCCESS(op, ##__VA_ARGS__)
 #else
  #define MO_ASSERT(condition, ...)
  #define MO_ASSERT_NON_NULL(ptr, ...)
